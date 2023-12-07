11c941dbe003c7463cfc6f3afba4a6b64f4b6e5f
wmajbour <wmajbour@hotmail.com>
2023-12-07 14:46:27 -0500

    Tests added for new controller methods

--- src/main/java/org/MiniSurveyMonkey/Controller/WebController.java
+++ src/main/java/org/MiniSurveyMonkey/Controller/WebController.java
@@ -50,9 +50,9 @@ public class WebController {
 
 
     @GetMapping("/user/showSurveys")
-    public String showSurveysForUser(Model model){
+    public String showSurveysForUser(Model model) {
         List<Survey> surveys = new ArrayList<>();
-        for (Survey survey : repository.findAll()){
+        for (Survey survey : repository.findAll()) {
             surveys.add(survey);
         }
         model.addAttribute("surveys", surveys);
@@ -60,14 +60,14 @@ public class WebController {
     }
 
     @GetMapping("/surveyor/PrintSurveys/surveyorPreview")
-    public String surveyorPreview(Model model){
+    public String surveyorPreview(Model model) {
         Survey survey = repository.findById(2);
         model.addAttribute(survey);
         return "SurveyPreviewView";
     }
 
     @GetMapping("/user/surveyView")
-    public String answerSurvey(Model model, @PathVariable int surveyId){
+    public String answerSurvey(Model model, @PathVariable int surveyId) {
         Survey survey = repository.findById(surveyId);
         model.addAttribute(survey);
         return "SurveyView";
@@ -80,9 +80,9 @@ public class WebController {
     }
 
     @GetMapping("/surveyor/PrintSurveys")
-    public String showCurrentSurveys(Model model){
+    public String showCurrentSurveys(Model model) {
         List<Survey> surveys = new ArrayList<>();
-        for (Survey survey : repository.findAll()){
+        for (Survey survey : repository.findAll()) {
             surveys.add(survey);
         }
         model.addAttribute("surveys", surveys);
@@ -97,11 +97,11 @@ public class WebController {
 
     @PostMapping("/surveyor/SurveyCreator/addmcq")
     public String addMcq(Model model, @RequestParam("mcq") String question,
-            @RequestParam("mcq1") String choice1,
-            @RequestParam("mcq2") String choice2,
-            @RequestParam("mcq3") String choice3,
-            @RequestParam("mcq4") String choice4,
-            @RequestParam("surveyId") Integer surveyId) {
+                         @RequestParam("mcq1") String choice1,
+                         @RequestParam("mcq2") String choice2,
+                         @RequestParam("mcq3") String choice3,
+                         @RequestParam("mcq4") String choice4,
+                         @RequestParam("surveyId") Integer surveyId) {
         Set<Choice> choices = new HashSet<>();
         MultipleChoice mcq = new MultipleChoice(question, choices);
         choices.add(new Choice(choice1, false));
@@ -118,9 +118,9 @@ public class WebController {
     @Transactional
     @PostMapping("/surveyor/SurveyCreator/addnrq")
     public String addNrq(Model model, @RequestParam("nrq") String question,
-            @RequestParam("min") int min,
-            @RequestParam("max") int max,
-            @RequestParam("surveyId") Integer surveyId) {
+                         @RequestParam("min") int min,
+                         @RequestParam("max") int max,
+                         @RequestParam("surveyId") Integer surveyId) {
 
         NumRange nrq = new NumRange(question, min, max);
         Survey survey = repository.findById(surveyId).orElse(null);
@@ -132,7 +132,7 @@ public class WebController {
 
     @PostMapping("/surveyor/SurveyCreator/addoeq")
     public String addOeq(Model model, @RequestParam("oeq") String question,
-            @RequestParam("surveyId") Integer surveyId) {
+                         @RequestParam("surveyId") Integer surveyId) {
         OpenEnded oeq = new OpenEnded(question);
         Survey survey = repository.findById(surveyId).orElse(null);
         survey.addQuestion(oeq);
@@ -153,14 +153,8 @@ public class WebController {
     @ResponseBody
     public ResponseEntity<String> closeAndSaveSurvey(@PathVariable(value = "surveyId") int surveyId) {
         Survey survey = repository.findById(surveyId);
-        if (survey != null) {
-            survey.close();
-            repository.save(survey);
-            return ResponseEntity.ok("Survey closed successfully.");
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Survey not found.");
-        }
+        survey.close();
+        repository.save(survey);
+        return ResponseEntity.ok("Survey closed successfully.");
     }
-
-
 }
