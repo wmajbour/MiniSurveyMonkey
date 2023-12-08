db2625a29bfe7dc80edf7a282b02159ed2e62e52
marinaiskander <marinalatif@cmail.carleton.ca>
2023-12-07 19:34:11 -0500

    Added tests for Webcontroller

--- src/test/java/WebControllerTest.java
+++ src/test/java/WebControllerTest.java
@@ -1,28 +1,23 @@
-import jakarta.websocket.OnOpen;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import static org.hamcrest.Matchers.*;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import org.MiniSurveyMonkey.Controller.WebController;
-import org.MiniSurveyMonkey.Model.Choice;
-import org.MiniSurveyMonkey.Model.MultipleChoice;
-import org.MiniSurveyMonkey.Model.OpenEnded;
 import org.MiniSurveyMonkey.Repo.MultipleChoiceRepository;
 import org.MiniSurveyMonkey.Repo.NumRangeRepository;
 import org.MiniSurveyMonkey.Repo.OpenEndedRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.MiniSurveyMonkey.Model.Survey;
 import org.MiniSurveyMonkey.Repo.SurveyRepository;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
 import java.util.*;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -45,108 +40,106 @@ public class WebControllerTest {
     @MockBean
     private OpenEndedRepository oeqRepo;
 
+    @Test
+    public void testShowHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
 
+
+    @Test
+    public void testShowSurveysForUser() throws Exception {
+        // Setup mock behavior for this test
+        List<Survey> mockSurveys = new ArrayList<>();
+        mockSurveys.add(new Survey());
+        when(repository.findAll()).thenReturn(mockSurveys);
+
+        mockMvc.perform(get("/user/showSurveys"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("PrintSurveysForUser"))
+                .andExpect(model().attributeExists("surveys"));
+    }
 
     @Test
     public void testGetAllSurveys() throws Exception {
-        // Arrange
-        Survey survey = new Survey();
-        survey.setId(1);
-        survey.setName("Sample Survey");
+        // Prepare mock data
+        List<Survey> mockSurveys = new ArrayList<>();
+        mockSurveys.add(new Survey());
+        mockSurveys.add(new Survey());
+        when(repository.findAll()).thenReturn(mockSurveys);
 
-        var set1 = new HashSet<Choice>();
-        var set2 = new HashSet<Choice>();
-        set1.add(new Choice("Year 1",false));
-        set1.add(new Choice("Year 2",false));
-        set1.add(new Choice("Year 3",false));
-        set1.add(new Choice("Year 4",false));
-        set2.add(new Choice("Spring",false));
-        set2.add(new Choice("Summer",false));
-        set2.add(new Choice("Fall",false));
-        set2.add(new Choice("Winter",false));
-
-        // Stubbing
-        MultipleChoice mcq1 = new MultipleChoice("Which was your favourite year of University?",
-                set1);
-        MultipleChoice mcq2 = new MultipleChoice("What season of the year is your favourite?",
-                set2);
-        repository.save(survey);
-
-        // Act & Assert
-        mockMvc.perform(MockMvcRequestBuilders.get("/surveyor/PrintSurveys"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("PrintSurveys"))
-                .andExpect(MockMvcResultMatchers.model().attribute("surveys", Collections.singletonList(survey)));
+        // Perform the request and assert results
+        mockMvc.perform(get("/user/showSurveys"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("PrintSurveysForUser"))
+                .andExpect(model().attribute("surveys", hasSize(2)))
+                .andExpect(model().attribute("surveys", is(mockSurveys)));
     }
 
-    @Test
-    public void testShowAddSurveyForm() throws Exception {
-        // Act & Assert
-        mockMvc.perform(MockMvcRequestBuilders.get("/surveys/add"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("addSurvey"));
-    }
+
+
+
 
     @Test
-    public void testAddSurvey() throws Exception {
-        // Act & Assert
-        mockMvc.perform(MockMvcRequestBuilders.post("/surveys/SurveyCreator/addSurvey")
-                        .param("name", "New Survey"))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.redirectedUrl("/surveys"));
-
-        // Verify that repository.save was called with the expected arguments
-        verify(repository).save(new Survey("New Survey"));
-    }
-
-    @Test
-    public void testSurveyorPreview() throws Exception {
-        // Arrange
-        Survey survey = new Survey();
-        survey.setId(1);
-        survey.setName("Sample Survey");
-        repository.save(survey);
-
-        // Act & Assert
-        mockMvc.perform(MockMvcRequestBuilders.get("/surveyor/PrintSurveys/surveyorPreview"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("SurveyPreviewView"))
-                .andExpect(MockMvcResultMatchers.model().attribute("survey", survey));
-    }
-
-    @Test
-    public void testAnswerSurvey() throws Exception {
-        // Arrange
+    public void testDeleteSurvey() throws Exception {
         int surveyId = 1;
-        Survey survey = new Survey();
-        survey.setId(surveyId);
-        survey.setName("Sample Survey");
-        repository.save(survey);
+        mockMvc.perform(delete("/survey/" + surveyId + "/close"))
+                .andExpect(status().isOk());
 
-        // Act & Assert
-        mockMvc.perform(MockMvcRequestBuilders.get("/user/surveyView/{surveyId}", surveyId))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("SurveyView"))
-                .andExpect(MockMvcResultMatchers.model().attribute("survey", survey));
+        verify(repository, times(1)).deleteById(surveyId);
     }
+
+
+
+
+
+
+
 
 
     @Test
     public void testCloseAndSaveSurvey() throws Exception {
-        // Arrange
         int surveyId = 1;
-        Survey survey = new Survey();
-        survey.setId(surveyId);
-        survey.setName("Sample Survey");
-        repository.save(survey);
+        Survey survey = new Survey("Sample Survey");
+        survey.setId(surveyId); // Set an ID for the survey
 
-        // Act & Assert
-        mockMvc.perform(MockMvcRequestBuilders.post("/surveyor/{surveyId}/close", surveyId))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+        // Mock the repository to return the survey when findById is called
+        when(repository.findById(surveyId)).thenReturn(survey);
 
+        mockMvc.perform(post("/surveyor/" + surveyId + "/close"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("SurveyResults"));
 
-        Optional<Survey> closedSurvey = Optional.ofNullable(repository.findById(surveyId));
+        verify(repository, times(1)).save(survey);
+        assertFalse(survey.isOpen());
+    }
+    @Test
+    public void testSurveyorPreview() throws Exception {
+        int surveyId = 1;
+        Survey survey = new Survey("Sample Survey");
+        when(repository.findById(surveyId)).thenReturn(survey);
+
+        mockMvc.perform(get("/surveyor/PrintSurveys/surveyorPreview"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("SurveyPreviewView"))
+                .andExpect(model().attribute("survey", survey));
+    }
+    @Test
+    public void testAddSurvey() throws Exception {
+        String surveyName = "New Survey";
+
+        mockMvc.perform(post("/surveyor/SurveyCreator/addSurvey")
+                        .param("surveyName", surveyName))
+                .andExpect(status().isOk())
+                .andExpect(view().name("SurveyAddQuestions"));
+
 
     }
+
+
+
+
+
 }
 
